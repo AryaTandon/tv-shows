7c737f0b8791a77bb54703ce6e305a230cb57a58
Abdi~ <kaliq.i@outlook.com>
2021-02-18 14:22:39 +0000

    up to summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import episodes from './episodes.json';
+import Episode from './Components/episode'
 
+interface IEpisode {
+  id: number;
+  url: string;
+  name: string;
+  season: number;
+  number: number;
+  type: string;
+  airdate: string;
+  airtime: string;
+  airstamp: string;
+  runtime: number;
+  image: {
+    medium: string;
+    original: string;
+  };
+  summary: string;
+  _links: { self: { href: string } };
+}
 function App() {
+
+  const mapEpisodeDetails = ({name, season, number, image, summary}:IEpisode) => {
+    return <Episode 
+            name={name}
+            season={season}
+            number={number}
+            image= {image}
+            summary = {summary}
+            />
+  }
+
+  let mappedEpisodes = episodes.map(mapEpisodeDetails)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     {mappedEpisodes}
     </div>
   );
 }
---
+++ src/Components/episode.tsx
@@ -0,0 +1,37 @@
+interface IEpisode {
+    // id: number;
+    // url: string;
+    name: string;
+    season: number;
+    number: number;
+    // type: string;
+    // airdate: string;
+    // airtime: string;
+    // airstamp: string;
+    // runtime: number;
+    image: {
+      medium: string;
+      original: string;
+    };
+    summary: string;
+    // _links: { self: { href: string } };
+  }
+
+const Episode = ({name, season,number, image, summary}:IEpisode) => {
+    
+    const cleanedSummary = summary.replace(/<p>|<\/p>|<br>/g, "") //figure out how to remove all <>
+
+
+    return (
+        <div>
+            <p>{name}
+             <span> S{season<10?`0${season}`:season}</span> 
+             <span>E{number<10?`0${number}`:number}</span>
+            </p>
+            <img src={image.medium} alt="episode photo"/>
+            <p>{cleanedSummary}</p>
+        </div>
+    )
+}
+
+export default Episode;
